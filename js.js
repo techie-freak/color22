d1861ab6685a4668c267593777a477bd7b487f8c
techie-freak <89081906+techie-freak@users.noreply.github.com>
2021-08-18 20:39:38 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function makeRed(){
+    document.body.style.backgroundColor="red";
+}
+function makegreen(){
+    document.body.style.backgroundColor="green";
+}
+function makepink(){
+    document.body.style.backgroundColor="pink";
+}
+function makeorange(){
+    document.body.style.backgroundColor="orange";
+}
